c29c1a28cc596ecf76d233da37fb147c1be240d9
Diego G.G <dieinmort@gmail.com>
2018-02-16 14:27:35 -0600

    EJS template manager

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
+//var path = require('path');
 
 app.use(express.static('public'));
+app.use(express.static('views'));
 
-var path = require('path');
+app.set("view engine", "ejs");
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+app.get('*', (req, response) => {
+    response.render("page", {data: req.url});
 });
 
+//app.get('/', function (req, res) {
+//    res.sendFile(path.join(__dirname + '/index.html'));
+//});
+app.listen(8888, () => console.log('Listening on port 8888'));
 
-app.listen(8888, () => console.log('Example app listening on port 8888'));
