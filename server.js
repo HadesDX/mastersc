ac15782cc21b1abaef2f575cd2d4f1345e8a03e7
Diego G.G <dieinmort@gmail.com>
2018-02-22 20:35:21 -0600

    File upload+info file post

--- server.js
+++ server.js
@@ -1,24 +1,85 @@
 const express = require('express');
 const parser = require('body-parser');
+const multiparty = require('multiparty');
+const fs = require('fs');
 const app = express();
-//var path = require('path');
+const path = require('path');
+
 var jparser = parser.json();
-var urlencodedParser = parser.urlencoded({ extended: false });
+var urlencodedParser = parser.urlencoded({extended: false});
 
 app.use(express.static('public'));
 app.use(express.static('views'));
 
 app.set("view engine", "ejs");
 
-app.get('*', (req, response) => {
-    response.render("page", {data: req.url});
+app.get('*', (req, res) => {
+    res.render("page", {data: req.url});
 });
 
-app.post('/postdata', urlencodedParser, (req, response) => {
+app.post('/postdata', urlencodedParser, (req, res) => {
     if (!req.body) {
-        return response.sendStatus(404);
+        return res.sendStatus(404);
     }
-    response.render("postdata", {data: req.body});
+    res.render("postdata", {data: req.body});
+});
+
+app.post('/postdata2', (req, res) => {
+    var form = new multiparty.Form();
+    var data = {data: {}};
+
+    form.parse(req, function (err, fields, files) {
+        if (err) {
+            console.log(err);
+            data.data.err = err;
+            res.render("postdata2", data);
+            return;
+        }
+
+        //No valid file?
+        if (!files['archivo'][0].originalFilename || files['archivo'][0].originalFilename === '') {
+            fs.unlink(files['archivo'][0].path, (err) => {
+                if (err) {
+                    console.warn(err, ' File not deleted: ' + files['archivo'][0].path);
+                }
+                console.log('Temporal file ' + files['archivo'][0].path + ' was deleted');
+            });
+            data.data.noFile = 'No valid file';
+            res.render("postdata2", data);
+            return;
+        }
+
+        //Copy uploaded file
+        var ext = /(\.[\w\d-]*)$/g.exec(files['archivo'][0].originalFilename);
+        ext ? ext = ext[0] : ext = '';
+        var filename = (new Date()).getTime();
+        var fullfile = path.join(__dirname, 'public', 'archivos', filename + ext);
+        var fullfileinfo = path.join(__dirname, 'public', 'archivos', filename + '.txt');
+        fs.writeFileSync(fullfile, fs.readFileSync(files['archivo'][0].path));
+
+        //Build up info file
+        var info = '';
+        for (var i in fields) {
+            data.data[i] = fields[i];
+            info += i + ' : ' + fields[i] + '\n';
+        }
+        fs.writeFileSync(fullfileinfo, info);
+
+        //Prepare for view
+        data.data.file = filename + ext;
+        data.data.fileInfo = filename + '.txt';
+
+        //Clean up ur dirty work
+        fs.unlink(files['archivo'][0].path, (err) => {
+            if (err) {
+                console.warn(err, ' File not deleted: ' + files['archivo'][0].path);
+            } else {
+                console.log('Temporal file ' + files['archivo'][0].path + ' was deleted');
+            }
+        });
+
+        res.render("postdata2", data);
+    });
 });
 
 //app.get('/', function (req, res) {
