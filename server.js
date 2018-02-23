d1efa6aba3b0d1d28083c0f212fb75527ccd7535
Diego G.G <dieinmort@gmail.com>
2018-02-22 18:37:42 -0600

    Avance de clase 17/02/2018

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const parser = require('body-parser');
 const app = express();
 //var path = require('path');
+var jparser = parser.json();
+var urlencodedParser = parser.urlencoded({ extended: false });
 
 app.use(express.static('public'));
 app.use(express.static('views'));
@@ -9,6 +12,13 @@ app.set("view engine", "ejs");
 
 app.get('*', (req, response) => {
     response.render("page", {data: req.url});
+});
+
+app.post('/postdata', urlencodedParser, (req, response) => {
+    if (!req.body) {
+        return response.sendStatus(404);
+    }
+    response.render("postdata", {data: req.body});
 });
 
 //app.get('/', function (req, res) {
